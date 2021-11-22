3e0af0cd6870c23eab78181aa7cd8ddf077fab8e
Garrett <garrett1440@gmail.com>
2021-11-22 14:55:44 -0600

    working reader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+
+export default class App extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      port: '',
+      reading: false,
+    }
+  }
+
+  componentDidMount() {
+    //  Maker sure the browser supports the web-serial-api
+    if (!'serial' in navigator) {
+      alert('Serial connections are not supported in your browser.')
+    } else {
+      // navigator.serial.addEventListener('connect', (e) => {
+      //   alert("Connected");
+      // });
+    }
+  }
+
+  readPort = async () => {
+
+    if (!this.state.reading) {
+      this.setState({
+        reading: true,
+      })
+    } else if (this.state.reading) {
+      this.setState({
+        reading: false,
+      })
+      return;
+    }
+
+    while(this.state.port.readable) {
+      let decoder = new TextDecoder();
+      const reader = this.state.port.readable.getReader()
+      try {
+        while (true) {
+          const { value, done } = await reader.read();
+          if (done) {
+            reader.releaseLock();
+            break;
+          }
+          console.log(decoder.decode(value))
+        }
+      } catch (error) {
+
+      } finally {
+        reader.releaseLock();
+      }
+    }
+  }
+
+  handlePorts = async () => {
+
+    //  Connect
+    if (!this.state.connected) {
+      await navigator.serial.requestPort().then((comPort) => {
+        this.setState({
+          port: comPort,
+          connected: true,
+        });
+        comPort.open({ baudRate: 9600 }).then((e) => {
+          alert('Opened')
+        });
+      });
+    //  Disconnect
+    } else {  
+      await this.state.port.close().then(() => {
+        this.setState({
+          port: '',
+          connected: false,
+          reading: false,
+        });
+        alert('Closed')
+      });
+    }
+
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>
+          {this.state.connected
+            ? this.state.port.getInfo().usbVendorId
+            : "Not connected"}
+        </h1>
+        <button onClick={this.handlePorts}>
+          {this.state.connected ? "Disconnect" : "Connect"}
+        </button>
+        <br />
+        <button onClick={this.readPort}>
+          {this.state.reading ? "Stop" : "Start"}
+        </button>
+        <br />
+        <br />
+      </div>
+    );
+  }
 }
-
-export default App;
